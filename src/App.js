0afa1d539013e67f8bd9c3784aa66f8e7b2bddad
Nauman <naumanumer10@outlook.com>
2023-10-17 00:11:01 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "font-awesome/css/font-awesome.min.css"
+import React from 'react'
+import Register from "./Register/Register"
+const App = () => {
+  return (
+    <>
+    <Register/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Register/Register.jsx
@@ -0,0 +1,93 @@
+import { Container ,Form,Modal,Button,Table} from 'react-bootstrap'
+import React from 'react'
+import './Register.css'
+const Register = () => {
+    
+  return (
+    <>
+    <Container className='py-4'>
+        <h1 className='text-center fw-bold'>Crud Operation in React JS</h1>
+    
+    <Button className='bg-danger border-0 btn-design'>
+        <i className='fa fa-plus'></i>
+    </Button>
+    <Table striped border hover className='mt-5 text-center'>
+        <thead>
+            <tr>
+                <th>Sr</th>
+                <th>Profile</th>
+                <th>Name</th>
+                <th>Mobile</th>
+                <th>Email</th>
+                <th>Password</th>
+            </tr>
+        </thead>
+        <tbody>
+            <td>1</td>
+            <td></td>
+            <td>nauman</td>
+            <td>12345</td>
+            <td>nauman@gmail.com</td>
+            <td>12345</td>
+        </tbody>
+
+    </Table>
+    <Modal show>
+        <Modal.Header closeButton>
+           < Modal.Title >
+           New Registration
+
+           </Modal.Title>
+           </Modal.Header>
+           <Modal.Body>
+            <Form>
+                <Form.Group className='mb-3'>
+                     <Form.Label>
+                        Name
+                       </Form.Label>
+                    <Form.Control />
+                       
+                    
+
+                </Form.Group>
+                <Form.Group className='mb-3'>
+                     <Form.Label>
+                       Name
+                       </Form.Label>
+                    <Form.Control />
+                       
+                    
+
+                </Form.Group>
+                <Form.Group className='mb-3'>
+                     <Form.Label>
+                       Password
+                       </Form.Label>
+                    <Form.Control />
+                       
+                    
+
+                </Form.Group>
+                <Form.Group className='mb-3'>
+                     <Form.Label >
+                      Profile
+                       </Form.Label>
+                    <Form.Control type="file" />
+                       
+                    
+
+                </Form.Group>
+                <Button className='w-100 mt-2 mb-3'>Regsiter</Button>
+            </Form>
+           </Modal.Body>
+
+        
+
+    </Modal>
+    </Container>
+    
+    </>
+  )
+}
+
+export default Register
